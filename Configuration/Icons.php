5d8a4629fbba16b95a6a02b8755be8c3d609959b
gdpr-extensions-com <support@gdpr-extensions.com>
2025-02-04 10:58:27 +0500

    Updated extension logo

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,7 +3,7 @@
 return [
     'gdpr_extensions_com_pint_board-plugin-gdprpinterest' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:gdpr_extensions_com_pint_board/Resources/Public/Icons/Pinterest-Logo.svg'
+        'source' => 'EXT:gdpr_extensions_com_pint_board/Resources/Public/Icons/Pinterest-Board-Widget.svg'
     ],
     'gdpr_extensions_com_tab' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
